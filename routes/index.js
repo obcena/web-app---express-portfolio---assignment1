8b3a74ca2f141938936ec4137e41f1fa690de25d
aobcena <aobcena@my.centennicalcollege.ca>
2022-10-02 22:00:50 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+
+/* GET contacts page. */
+router.get('/contacts', function(req, res, next) {
+  res.render('index', { title: 'Contacts' });
+});
+
+module.exports = router;
